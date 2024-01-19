55a31eaad0594d6309e283355b285082ad7c61df
ThiepLH <khucmohoang2004@gmail.com>
2024-01-19 18:38:45 +0700

    update logo

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,86 +1,97 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
 import AppStyle from '../theme';
-import {
-    View,
-    Image,
-    BackHandler,
-} from "react-native";
-import RegisterForm from "../components/RegisterForm";
-import { useNavigation } from "@react-navigation/native";
-import SwitchSelector from "react-native-switch-selector";
-import LoginForm from "../components/LoginForm";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import {View, Image, BackHandler} from 'react-native';
+import RegisterForm from '../components/RegisterForm';
+import {useNavigation} from '@react-navigation/native';
+import SwitchSelector from 'react-native-switch-selector';
+import LoginForm from '../components/LoginForm';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import ScaleImage from '../components/ScaleImage';
 export default function LoginPage() {
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [step, setStep] = useState('INPUT_PHONE_NUMBER');
-    const [loginResult, setLoginResult] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [step, setStep] = useState('INPUT_PHONE_NUMBER');
+  const [loginResult, setLoginResult] = useState(null);
 
+  const navigation = useNavigation<any>();
 
-    const navigation = useNavigation<any>();
-
-    function handleBackButtonClick() {
-        if (step == 'VERIFY_OTP') {
-            setStep('INPUT_PHONE_NUMBER');
-            return true;
-        }
-        return false;
+  function handleBackButtonClick() {
+    if (step == 'VERIFY_OTP') {
+      setStep('INPUT_PHONE_NUMBER');
+      return true;
     }
+    return false;
+  }
 
-    useEffect(() => {
-        BackHandler.addEventListener("hardwareBackPress", handleBackButtonClick);
-        return () => {
-            BackHandler.removeEventListener("hardwareBackPress", handleBackButtonClick);
-        };
-    }, []);
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackButtonClick);
+    return () => {
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        handleBackButtonClick,
+      );
+    };
+  }, []);
 
-    return (
-        <KeyboardAwareScrollView>
-            <View style={[AppStyle.StyleLogin.container, {
-                alignItems: "center",
-                justifyContent: "center",
-            }]}>
-                {(step === 'INPUT_PHONE_NUMBER' || step === 'VERIFY_SUCCESS') &&
-                    <>
-                        <Image style={AppStyle.StyleLogin.logo} source={require("../../assets/images/logo.png")} />
-                        <SwitchSelector
-                            initial={0}
-                            value={selectedIndex}
-                            onPress={(value: any) => setSelectedIndex(value)}
-                            textColor={"#000000"} //'#7a44cf'
-                            selectedColor={"#000000"}
-                            buttonColor={"#ffffff"}
-                            borderColor={"#EEEEEF"}
-                            borderRadius={8}
-                            hasPadding
-                            valuePadding={2}
-                            height={48}
-                            backgroundColor={"#EEEEEF"}
-                            options={[
-                                { label: "Sign In", value: 0 },
-                                { label: "Sign up", value: 1 }
-                            ]}
-                            testID="gender-switch-selector"
-                            accessibilityLabel="gender-switch-selector"
-                        />
+  return (
+    <KeyboardAwareScrollView>
+      <View
+        style={[
+          AppStyle.StyleLogin.container,
+          {
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        ]}>
+        {(step === 'INPUT_PHONE_NUMBER' || step === 'VERIFY_SUCCESS') && (
+          <>
+            <ScaleImage
+              style={AppStyle.StyleLogin.logo}
+              source={require('../../assets/images/logo.png')}
+            />
+            <SwitchSelector
+              initial={0}
+              value={selectedIndex}
+              onPress={(value: any) => setSelectedIndex(value)}
+              textColor={'#000000'} //'#7a44cf'
+              selectedColor={'#000000'}
+              buttonColor={'#ffffff'}
+              borderColor={'#EEEEEF'}
+              borderRadius={8}
+              hasPadding
+              valuePadding={2}
+              height={48}
+              backgroundColor={'#EEEEEF'}
+              options={[
+                {label: 'Sign In', value: 0},
+                {label: 'Sign up', value: 1},
+              ]}
+              testID="gender-switch-selector"
+              accessibilityLabel="gender-switch-selector"
+            />
 
-                        {selectedIndex == 0 ?
-                            <LoginForm requestSuccess={() => {
-                                navigation.navigate("OTPVerifyPage", { action: 'SIGNIN' });
-                            }} onLoginFailed={(result: any) => {
-                                setLoginResult(result);
-                                setSelectedIndex(1);
-                            }} />
-                            :
-                            <RegisterForm
-                                loginResult={loginResult}
-                                requestSuccess={() => {
-                                    navigation.navigate("OTPVerifyPage", {
-                                        action: 'REGISTER'
-                                    });
-                                }} />
-                        }
-                    </>
-                }
-            </View>
-        </KeyboardAwareScrollView>)
+            {selectedIndex == 0 ? (
+              <LoginForm
+                requestSuccess={() => {
+                  navigation.navigate('OTPVerifyPage', {action: 'SIGNIN'});
+                }}
+                onLoginFailed={(result: any) => {
+                  setLoginResult(result);
+                  setSelectedIndex(1);
+                }}
+              />
+            ) : (
+              <RegisterForm
+                loginResult={loginResult}
+                requestSuccess={() => {
+                  navigation.navigate('OTPVerifyPage', {
+                    action: 'REGISTER',
+                  });
+                }}
+              />
+            )}
+          </>
+        )}
+      </View>
+    </KeyboardAwareScrollView>
+  );
 }
--- src/theme/StyleLogin.tsx
+++ src/theme/StyleLogin.tsx
@@ -1,30 +1,31 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 const StyleLogin = StyleSheet.create({
   container: {
     padding: 20,
-    marginTop: 150,
-    backgroundColor: "#ffffff"
+    marginTop: 130,
+    backgroundColor: '#ffffff',
   },
   form: {
     marginTop: 20,
     alignSelf: 'stretch',
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logo: {
-    marginBottom: 100,
-    width: "60%"
+    alignSelf: 'center',
+    marginBottom: 40,
+    width: 256,
   },
   input: {
     borderRadius: 8,
-    width: "100%",
+    width: '100%',
     height: 48,
     marginBottom: 12,
-    backgroundColor: "#EEEEEE",
+    backgroundColor: '#EEEEEE',
   },
   TextInput: {
     marginLeft: 12,
-    color: "#4F4A45",
+    color: '#4F4A45',
   },
 });
 export default StyleLogin;
